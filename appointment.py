a6b79bc4dff317ef87efbe426f5c9ad3f617c537
Menaka Anandan <menakaanandan324@gmail.com>
2026-07-17 19:12:54 +0530

    Enhance appointment and login modules with functions

--- appointment.py
+++ appointment.py
@@ -1,2 +1,23 @@
-print("Hospital Appointment Booking System")
-print("Patient Registration Module - under development")
+patients=[]
+doctors=[
+    "dr.smith(cardiologist)",
+    "dr.johnson(dermatologist)",
+    "dr.williams(orthopedic)"
+]
+def register_patient():
+    name=input("enter patient name:")
+    age=input("enter age:")
+    patient ={"name":name,"age":age}
+    patients.append(patient)
+    print("patientregistered successfully!\n")
+def view_patients():
+    print("---registered patients ---")
+    if not patients:
+        print("no patients registered.\n")
+        return
+    for p in patients:
+        print(p)
+    
+def book_appoinment(patient_name,doctor_name,date):
+    print(f"appointmemnt booked for{patient_name}with {doctor_name} on {date}")
+print("hospital appointment booking system")
